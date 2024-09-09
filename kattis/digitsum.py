de9cea3d3a8f435577a4a5abe627602763aa2e99
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2024-09-09 14:41:03 -0400

    New digit sum

--- kattis/digitsum.py
+++ kattis/digitsum.py
@@ -1,42 +1,35 @@
-import math
+def digit_sum(n):
+    s = 0
+    while n > 0:
+        s += n % 10
+        n //= 10
+    return s
 
-def sumOfDigitsFrom1ToN(x, n):
-    if (n < 10):
-        return (n * (n + 1)) // 2 - (x * (x + 1)) // 2
-    d = int(math.log(n, 10))
-    a = [0]*(d + 2)
-    a[0] = 0
-    a[1] = 45
-    for i in range(2, d + 1):
-        a[i] = a[i - 1] * 10 + 45 * int(math.ceil(pow(10, i - 1)))
-    return sumOfDigitsFrom1ToNUtil(x, n, a)
- 
-def sumOfDigitsFrom1ToNUtil(x, n, a):
-    total = 0
-    count = 0
-    tx = 0
-    while True:
-        if (n < 10):
-            total += (n * (n + 1)) // 2
-            return total
-        d = int(math.log(n,10))
-        p = int(math.ceil(pow(10, d)))
-        msd = n // p 
-        total += int(msd * a[d] + (msd * (msd - 1) // 2) * p + msd * (1 + n % p)) 
-        n %= p
-        print(x, count)
-        if count == x - 1 and x > 1:
-            print(count)
-            total = 0
-        count += 1
 
-for i in range(int(input())):
-    whole = False
-    x, y = map(int, input().split())
-    if x == y:
-        print(y)
-        continue
-    if x == 0:
-        x += 1
-    totalA = sumOfDigitsFrom1ToN(x,y)
-    print(totalA)
+def digit_sum_precomp(limit):
+    # Precompute digit sums for all numbers up to 'limit'
+    digit_sums = {x:0 for x in range(limit+1)}
+    for i in range(1, limit + 1):
+        digit_sums[i] = digit_sums[i - 1] + digit_sum(i)
+    return digit_sums
+
+def main():
+    n = int(input())
+    ranges = [None for _ in range(n)]
+    ans = [None for _ in range(n)]
+    max_limit = 0 
+
+    for i in range(n):
+        x,y = map(int, input().split())
+        ranges[i] = (x,y)
+        max_limit = max(max_limit, y)
+
+    digit_sums = digit_sum_precomp(max_limit)
+
+    for x,y in ranges:
+        if x == 0:
+            print(digit_sums[y])
+        else:
+            print(digit_sums[y] - digit_sums[x-1])
+
+main()
