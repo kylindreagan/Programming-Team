7a5a470ae400273156a24d091f3cfa6255aa86bc
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2025-01-16 20:53:41 -0500

    A bit better neighborhood watch
    dict to bool for less overhead

--- kattis/neighborhoodwatch.py
+++ kattis/neighborhoodwatch.py
@@ -1,22 +1,23 @@
 from typing import List
-def calculate_safety_rating(n:int, k:int, watch_houses):
+def calculate_safety_rating(n:int, k:int, watch_houses: List[bool]):
     if k == 0:
         return 0  # No walk is safe if there are no watch houses
 
     safety_rating = n*(n+1)//2
     curr_unsafe = 0
-    for house in watch_houses.keys():
-        if watch_houses[house] == False:
-            safety_rating -= (1 + curr_unsafe)
+
+    for house in range(n):
+        if not watch_houses[house]:
             curr_unsafe += 1
+            safety_rating -= curr_unsafe
         else:
             curr_unsafe = 0
     return safety_rating
 
 n, k = map(int, input().split())
-watch_houses = {i+1:False for i in range(n)}
+watch_houses = [False for _ in range(n)]
 for i in range(k):
-    watch_houses[int(input())] = True
+    watch_houses[int(input())-1] = True
 
 print(calculate_safety_rating(n, k, watch_houses))
 
