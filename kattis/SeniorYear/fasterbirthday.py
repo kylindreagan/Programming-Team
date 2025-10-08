b1a99da99b9dced277caa0ec3c3f4d1673ef1505
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2025-10-08 16:44:49 -0400

    Better birthday party
    Uses tarjan

---
+++ kattis/SeniorYear/fasterbirthday.py
@@ -0,0 +1,43 @@
+#Assistance from https://www.geeksforgeeks.org/dsa/tarjan-algorithm-find-strongly-connected-components/
+def tarjandfs(u, parent):
+    visited[u] = True
+    disc[u] = low[u] = time[0]
+    time[0] += 1
+
+    for v in graph[u]:
+        if not visited[v]:
+            tarjandfs(v, u)
+            low[u] = min(low[u], low[v])
+
+            # bridge condition
+            if low[v] > disc[u]:
+                bridges.append((u, v))
+        elif v != parent:
+            low[u] = min(low[u], disc[v])
+
+while True:
+    p, c = map(int, input().split())
+    if p == 0 and c == 0:
+        break
+
+    graph = [[] for _ in range(p)]
+    for _ in range(c):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    visited = [False]*p
+    disc = [float('inf')]*p
+    low = [float('inf')]*p
+    time = [0]
+    bridges = []
+
+    for i in range(p):
+        if not visited[i]:
+            tarjandfs(i, -1)
+
+    print(bridges)
+    if bridges:
+        print("Yes")
+    else:
+        print("No")
