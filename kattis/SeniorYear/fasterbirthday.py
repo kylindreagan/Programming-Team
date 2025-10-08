93ac7c3c787ced89aeea978b5541d28fd234e44e
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2025-10-08 17:09:16 -0400

    fasterer birthday

--- kattis/SeniorYear/fasterbirthday.py
+++ kattis/SeniorYear/fasterbirthday.py
@@ -1,5 +1,5 @@
 #Assistance from https://www.geeksforgeeks.org/dsa/tarjan-algorithm-find-strongly-connected-components/
-def tarjandfs(u, parent):
+"""def tarjandfs(u, parent):
     visited[u] = True
     disc[u] = low[u] = time[0]
     time[0] += 1
@@ -38,4 +38,53 @@ while True:
     if bridges:
         print("Yes")
     else:
-        print("No")
+        print("No")"""
+
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+def solve():
+    while True:
+        p, c = map(int, input().split())
+        if p == 0 and c == 0:
+            break
+
+        graph = [[] for _ in range(p)]
+        for _ in range(c):
+            a, b = map(int, input().split())
+            graph[a].append(b)
+            graph[b].append(a)
+
+        visited = [False] * p
+        disc = [0] * p
+        low = [0] * p
+        time = 0
+        found = False
+
+        def dfs(u, parent):
+            nonlocal time, found
+            visited[u] = True
+            disc[u] = low[u] = time
+            time += 1
+
+            for v in graph[u]:
+                if found: return  # early exit
+                if not visited[v]:
+                    dfs(v, u)
+                    low[u] = min(low[u], low[v])
+                    if low[v] > disc[u]:
+                        found = True
+                        return
+                elif v != parent:
+                    low[u] = min(low[u], disc[v])
+
+        for i in range(p):
+            if not visited[i]:
+                dfs(i, -1)
+                if found:
+                    break
+
+        print("Yes" if found else "No")
+
+solve()
