439c6d9f2f4f5445936c30ba5f5fb623f4e9f38b
Kylind Reagan <kylindreagan@gmail.com>
2026-02-06 01:57:53 -0500

    Sovier find fix
    Actual optimized version

--- kattis/SeniorYear/sovietunionfind.py
+++ kattis/SeniorYear/sovietunionfind.py
@@ -27,45 +27,33 @@ class UFDS:
         
         self.parent[y] = x
         
-        tail_x = self.tail[x]
-        head_y = self.head[y]
-        
-        self.next[tail_x] = head_y
+        self.next[self.tail[x]] = self.head[y]
         self.tail[x] = self.tail[y]
         self.head[y] = -1
         self.tail[y] = -1
     
     def balkanize(self, x):
         root = self.find_set(x)
-        
+        h = self.head[root]
         if self.head[root] == root and self.tail[root] == root:
             return [x] if x == root else []
         
         members = []
-        current = self.head[root]
+        curr = h
         tail = self.tail[root]
-        
         while True:
-            members.append(current)
-            if current == tail:
+            members.append(curr)
+            if curr == tail:
                 break
-            current = self.next[current]
+            curr = self.next[curr]
         
-        parent = self.parent
-        head = self.head
-        tail_arr = self.tail
-        next_arr = self.next
-        n = len(parent)
-        
+        parent, head, tail_arr, next_arr, n = self.parent, self.head, self.tail, self.next, len(self.parent)
         for u in members:
-            parent[u] = u
-            head[u] = u
-            tail_arr[u] = u
+            parent[u] = head[u] = tail_arr[u] = u
             next_arr[u] = u + 1 if u + 1 < n else -1
         
         if x == root:
-            parent[x] = x
-            head[x] = x
+            parent[x] = head[x] = tail_arr[x] = x
             tail_arr[x] = x
             next_arr[x] = x + 1 if x + 1 < n else -1
         
@@ -73,33 +61,30 @@ class UFDS:
 
 def main():
     data = sys.stdin.buffer.read().split()
+    it = iter(data)
     
-    idx = 0
-    n, q = int(data[idx]), int(data[idx+1])
-    idx += 2
+    n = int(next(it))
+    q = int(next(it))
     
     ufds = UFDS(n)
     results = []
     results_append = results.append
     for _ in range(q):
-        cmd = data[idx]
-        idx += 1
+        cmd = next(it)
+        b = cmd[0]
         
-        if cmd[0] == 97:  # 'a' = 97 in ASCII
-            x = int(data[idx]) - 1
-            y = int(data[idx+1]) - 1
-            idx += 2
+        if b == 97:  # 'a' = 97 in ASCII
+            x = int(next(it)) - 1
+            y = int(next(it)) - 1
             ufds.union(x, y)
             
-        elif cmd[0] == 98:  # 'b' = 98 in ASCII
-            x = int(data[idx]) - 1
-            idx += 1
+        elif b == 98:  # 'b' = 98 in ASCII
+            x = int(next(it)) - 1
             ufds.balkanize(x)
             
         else:
-            x = int(data[idx]) - 1
-            idx += 1
-            results_append(str(ufds.get_ruler(x)))
+            x = int(next(it)) - 1
+            results_append(str(ufds.find_set(x) + 1))
     
     sys.stdout.write("\n".join(results))
 
