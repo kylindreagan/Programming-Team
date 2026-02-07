c93ba9119682a945d344b07a81c09b6a486b5f9b
Kylind Reagan <kylindreagan@gmail.com>
2026-02-07 01:04:30 -0500

    Completed exam manipulation

--- kattis/SeniorYear/exammanipulation.py
+++ kattis/SeniorYear/exammanipulation.py
@@ -1,16 +1,22 @@
-n,k = map(int, input().split())
-can_get = [True] * k
-init = input()
-for i in range(n-1):
-    test = input()
-    for i in range(len(test)):
-        if can_get[i]:
-            can_get[i] = test[i] == init[i]
-    if all(element == False for element in can_get):
-        print(0)
-        break
-total = 0
-for t in can_get:
-    if t:
-        total += 1
-print(total)
+n, k = map(int, input().split())
+students = [input().strip() for _ in range(n)]
+
+best = 0
+
+for mask in range(1 << k):
+    key = []
+    for i in range(k):
+        if mask & (1 << i):
+            key.append('T')
+        else:
+            key.append('F')
+    key = ''.join(key)
+
+    lowest = k
+    for s in students:
+        score = sum(1 for i in range(k) if s[i] == key[i])
+        lowest = min(lowest, score)
+
+    best = max(best, lowest)
+
+print(best)
